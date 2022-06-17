219a1a9bc32ef23cc1ef134bad59d2b00126c85f
balamurugangovindaraj <87898911+balamurugangovindaraj@users.noreply.github.com>
2022-06-17 15:16:05 +0200

    Delete index.js

--- backend/index.js
+++
@@ -1,8 +0,0 @@
-const express= require('express');
-const app=express();
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://node-api:1234@srh-adb-01.inyaq.mongodb.net/?retryWrites=true&w=majority').then(()=>{
-    console.log('Database Connected')
-}).catch(err=> {
-     console.log('Database not connected'+err)
-});
